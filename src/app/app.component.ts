24d5e9e272a9a5b2ce54d517e5058ad893ab029d
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-06-01 22:40:47 +0300

    Separate header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { AvailablePlacesComponent } from './places/available-places/available-places.component';
 import { UserPlacesComponent } from './places/user-places/user-places.component';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AvailablePlacesComponent, UserPlacesComponent],
+  imports: [AvailablePlacesComponent, UserPlacesComponent, HeaderComponent],
 })
 export class AppComponent {}
